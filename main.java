152d525530c980c3646c357b73bf5a30febc3d71
parminder911 <parminderj98@gmail.com>
2025-06-07 18:23:39 +0530

    Update main.java
    remove 2nd same march 2025

--- main.java
+++ main.java
@@ -3,6 +3,6 @@ public class Main {
         System.out.println("Hello, World!");
            System.out.println("This is my java program");
               System.out.println(" March 2025 ");
-               System.out.println(" March 2025 ");
+              
     }
 }
