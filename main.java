f0e22c5d067f23df56aa55b3c7baa3ba3fec489d
Parminder <84897471+parminder911@users.noreply.github.com>
2025-06-07 18:05:15 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+           System.out.println("This is my java program");
+              System.out.println(" March 2025 ");
+               System.out.println(" March 2025 ");
+    }
+}
